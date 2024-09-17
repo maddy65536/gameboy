4749fc028a58bcfb79976b72b23b8f42c5b0d315
maddy65536 <32717754+maddy65536@users.noreply.github.com>
2024-09-17 12:06:57 -0400

    this will probably end up being more consistent naming

--- src/cpu.rs
+++ src/cpu.rs
@@ -14,72 +14,72 @@ pub struct RegisterFile {
 
 impl RegisterFile {
     // r/w 16-bit registers
-    pub fn set_af(&mut self, val: u16) {
+    pub fn write_af(&mut self, val: u16) {
         self.a = ((val & 0xFF00) >> 8) as u8;
         self.f = (val & 0x00FF) as u8;
     }
 
-    pub fn get_af(&self) -> u16 {
+    pub fn read_af(&self) -> u16 {
         ((self.a as u16) << 8) | (self.f as u16)
     }
 
-    pub fn set_bc(&mut self, val: u16) {
+    pub fn write_bc(&mut self, val: u16) {
         self.b = ((val & 0xFF00) >> 8) as u8;
         self.c = (val & 0x00FF) as u8;
     }
 
-    pub fn get_bc(&self) -> u16 {
+    pub fn read_bc(&self) -> u16 {
         ((self.b as u16) << 8) | (self.c as u16)
     }
 
-    pub fn set_de(&mut self, val: u16) {
+    pub fn write_de(&mut self, val: u16) {
         self.d = ((val & 0xFF00) >> 8) as u8;
         self.e = (val & 0x00FF) as u8;
     }
 
-    pub fn get_de(&self) -> u16 {
+    pub fn read_de(&self) -> u16 {
         ((self.d as u16) << 8) | (self.e as u16)
     }
 
-    pub fn set_hl(&mut self, val: u16) {
+    pub fn write_hl(&mut self, val: u16) {
         self.h = ((val & 0xFF00) >> 8) as u8;
         self.l = (val & 0x00FF) as u8;
     }
 
-    pub fn get_hl(&self) -> u16 {
+    pub fn read_hl(&self) -> u16 {
         ((self.h as u16) << 8) | (self.l as u16)
     }
 
     // r/w flags
-    pub fn set_z(&mut self, val: bool) {
+    pub fn write_z(&mut self, val: bool) {
         self.f = (self.f & !(1 << 7)) | ((val as u8) << 7);
     }
 
-    pub fn get_z(&self) -> bool {
+    pub fn read_z(&self) -> bool {
         ((self.f & (1 << 7)) >> 7) == 1
     }
 
-    pub fn set_n(&mut self, val: bool) {
+    pub fn write_n(&mut self, val: bool) {
         self.f = (self.f & !(1 << 6)) | ((val as u8) << 6);
     }
 
-    pub fn get_n(&self) -> bool {
+    pub fn read_n(&self) -> bool {
         ((self.f & (1 << 6)) >> 6) == 1
     }
 
-    pub fn set_h(&mut self, val: bool) {
+    pub fn write_h(&mut self, val: bool) {
         self.f = (self.f & !(1 << 5)) | ((val as u8) << 5);
     }
 
-    pub fn get_h(&self) -> bool {
+    pub fn read_h(&self) -> bool {
         ((self.f & (1 << 5)) >> 5) == 1
     }
 
-    pub fn set_c(&mut self, val: bool) {
+    pub fn write_c(&mut self, val: bool) {
         self.f = (self.f & !(1 << 4)) | ((val as u8) << 4);
     }
 
-    pub fn get_c(&self) -> bool {
+    pub fn read_c(&self) -> bool {
         ((self.f & (1 << 4)) >> 4) == 1
     }
 }
