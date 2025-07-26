0b4d868b43d6435f831cbd69b9832f12c16cf0f2
maddy65536 <32717754+maddy65536@users.noreply.github.com>
2025-07-26 09:37:41 -0400

    bus comment tweaks

--- src/bus.rs
+++ src/bus.rs
@@ -6,7 +6,7 @@ use crate::serial::Serial;
 use crate::timer::Timer;
 
 pub struct Bus {
-    pub ram: [u8; 0x10000], // just a flat array until i start the memory map stuff
+    pub ram: [u8; 0x10000], // underlying array for plain memory accesses
     pub cart: Box<dyn Mbc>,
     pub timer: Timer,
     pub ppu: Ppu,
@@ -58,40 +58,6 @@ impl Bus {
         self.ram[addr as usize] = val;
     }
 
-    // pub fn read_u8(&self, addr: u16) -> u8 {
-    //     match addr {
-    //         0x0000..=0x3FFF => self.cart.read_u8(addr),
-    //         0x4000..=0x7FFF => self.cart.read_u8(addr),
-    //         0x8000..=0x9FFF => unimplemented!("tried to read vram"),
-    //         0xA000..=0xBFFF => unimplemented!("tried to read cart ram"),
-    //         0xC000..=0xCFFF => self.ram_read(addr),
-    //         0xD000..=0xDFFF => self.ram_read(addr),
-    //         0xE000..=0xFDFF => unimplemented!("tried to read echo ram"),
-    //         0xFE00..=0xFE9F => unimplemented!("tried to read OAM"),
-    //         0xFEA0..=0xFEFF => unimplemented!("tried to read FORBIDDEN MEMORY"),
-    //         0xFF00..=0xFF7F => self.io_read_u8(addr),
-    //         0xFF80..=0xFFFE => self.ram_read(addr), // HRAM
-    //         0xFFFF => self.ram_read(addr),          // IE
-    //     }
-    // }
-
-    // pub fn write_u8(&mut self, addr: u16, val: u8) {
-    //     match addr {
-    //         0x0000..=0x3FFF => unimplemented!("tried to write to cart rom bank 0"),
-    //         0x4000..=0x7FFF => unimplemented!("tried to write to cart rom bank 01-NN"),
-    //         0x8000..=0x9FFF => unimplemented!("tried to write to vram"),
-    //         0xA000..=0xBFFF => unimplemented!("tried to write to cart ram"),
-    //         0xC000..=0xCFFF => self.ram_write(addr, val),
-    //         0xD000..=0xDFFF => self.ram_write(addr, val),
-    //         0xE000..=0xFDFF => unimplemented!("tried to write to echo ram"),
-    //         0xFE00..=0xFE9F => unimplemented!("tried to write to OAM"),
-    //         0xFEA0..=0xFEFF => unimplemented!("tried to write to FORBIDDEN MEMORY"),
-    //         0xFF00..=0xFF7F => self.io_write_u8(addr, val),
-    //         0xFF80..=0xFFFE => self.ram_write(addr, val), // HRAM
-    //         0xFFFF => self.ram_write(addr, val),          // IE
-    //     }
-    // }
-
     pub fn read_u8(&self, addr: u16) -> u8 {
         match addr {
             0x0000..=0x3FFF => self.cart.read_u8(addr), // cart rom bank 0
@@ -100,10 +66,12 @@ impl Bus {
             0xA000..=0xBFFF => self.cart.read_u8(addr), // external RAM
             0xC000..=0xCFFF => self.ram_read(addr),
             0xD000..=0xDFFF => self.ram_read(addr),
+            // 0xE000..=0xFDFF => unimplemented!("tried to read echo ram"),
             0xFE00..=0xFE9F => self.ppu.read_u8(addr), // OAM
-            0xFF00..=0xFF7F => self.io_read_u8(addr),  // IO
-            0xFF80..=0xFFFE => self.ram_read(addr),    // HRAM
-            0xFFFF => self.ram_read(addr),             // IE
+            // 0xFEA0..=0xFEFF => unimplemented!("tried to read FORBIDDEN MEMORY"),
+            0xFF00..=0xFF7F => self.io_read_u8(addr), // IO
+            0xFF80..=0xFFFE => self.ram_read(addr),   // HRAM
+            0xFFFF => self.ram_read(addr),            // IE
             _ => self.ram_read(addr),
         }
     }
@@ -116,10 +84,12 @@ impl Bus {
             0xA000..=0xBFFF => self.cart.write_u8(addr, val), // external RAM
             0xC000..=0xCFFF => self.ram_write(addr, val),
             0xD000..=0xDFFF => self.ram_write(addr, val),
+            // 0xE000..=0xFDFF => unimplemented!("tried to write to echo ram"),
             0xFE00..=0xFE9F => self.ppu.write_u8(addr, val), // OAM
-            0xFF00..=0xFF7F => self.io_write_u8(addr, val),  // IO
-            0xFF80..=0xFFFE => self.ram_write(addr, val),    // HRAM
-            0xFFFF => self.ram_write(addr, val),             // IE
+            // 0xFEA0..=0xFEFF => unimplemented!("tried to write to FORBIDDEN MEMORY"),
+            0xFF00..=0xFF7F => self.io_write_u8(addr, val), // IO
+            0xFF80..=0xFFFE => self.ram_write(addr, val),   // HRAM
+            0xFFFF => self.ram_write(addr, val),            // IE
             _ => self.ram_write(addr, val),
         }
     }
@@ -133,60 +103,21 @@ impl Bus {
         self.write_u8(addr + 1, ((val & 0xFF00) >> 8) as u8);
     }
 
-    // fn io_read_u8(&self, addr: u16) -> u8 {
-    //     match addr {
-    //         0xFF00 => unimplemented!("tried to read joypad input"),
-    //         0xFF01 => self.ram_read(addr), // serial data
-    //         0xFF02 => unimplemented!("tried to read serial transfer"),
-    //         0xFF04..=0xFF07 => self.timer.read_u8(addr),
-    //         0xFF0F => self.ram_read(addr), // IF
-    //         0xFF10..=0xFF26 => unimplemented!("tried to read audio register"),
-    //         0xFF30..=0xFF3F => unimplemented!("tried to read wave pattern ram"),
-    //         0xFF40..=0xFF4B => unimplemented!("tried to read LCD control"),
-    //         0xFF4F => unimplemented!("tried to read CGB VRAM bank select"),
-    //         0xFF50 => unimplemented!("tried to read boot rom flag"),
-    //         0xFF51..=0xFF55 => unimplemented!("tried to read CGB VRAM DMA"),
-    //         0xFF68..=0xFF6B => unimplemented!("tried to read CGB BG/OBJ palettes"),
-    //         0xFF70 => unimplemented!("tried to read CGB WRAM bank select"),
-    //         _ => unimplemented!("tried to read invalid i/o register {:#06X}????", addr),
-    //     }
-    // }
-
-    // fn io_write_u8(&mut self, addr: u16, val: u8) {
-    //     match addr {
-    //         0xFF00 => unimplemented!("tried to write to joypad input"),
-    //         0xFF01 => self.ram_write(addr, val), // serial data
-    //         0xFF02 => {
-    //             if val == 0x81 {
-    //                 print!("{}", self.ram[0xFF01] as char)
-    //             } else {
-    //                 unimplemented!("tried something weird with serial, wrote {:#04X}", val)
-    //             }
-    //         }
-    //         0xFF04..=0xFF07 => self.timer.write_u8(addr, val),
-    //         0xFF0F => self.ram_write(addr, val), // IF
-    //         0xFF10..=0xFF26 => unimplemented!("tried to write to audio register"),
-    //         0xFF30..=0xFF3F => unimplemented!("tried to write to wave pattern ram"),
-    //         0xFF40..=0xFF4B => unimplemented!("tried to write to LCD control"),
-    //         0xFF4F => unimplemented!("tried to write to CGB VRAM bank select"),
-    //         0xFF50 => unimplemented!("tried to write to boot rom flag"),
-    //         0xFF51..=0xFF55 => unimplemented!("tried to write to CGB VRAM DMA"),
-    //         0xFF68..=0xFF6B => unimplemented!("tried to write to CGB BG/OBJ palettes"),
-    //         0xFF70 => unimplemented!("tried to write to CGB WRAM bank select"),
-    //         _ => unimplemented!("tried to write to invalid i/o register {:#06X}????", addr),
-    //     }
-    // }
-
     fn io_read_u8(&self, addr: u16) -> u8 {
         match addr {
             0xFF00 => self.joypad.read_u8(),              // joypad
             0xFF01 | 0xFF02 => self.serial.read_u8(addr), // serial data
-            0xFF04..=0xFF07 => self.timer.read_u8(addr),
-            0xFF10..=0xFF26 => self.apu.read_u8(addr),
-            0xFF30..=0xFF3F => self.apu.read_u8(addr),
-            0xFF46 => 0, // what does reading from the dma register do?
-            0xFF40..0xFF46 | 0xFF47..=0xFF4B => self.ppu.read_u8(addr),
-            0xFF0F => self.ram_read(addr), // IF
+            0xFF04..=0xFF07 => self.timer.read_u8(addr),  // timer
+            0xFF0F => self.ram_read(addr),                // IF
+            0xFF10..=0xFF14 | 0xFF16..=0xFF1E | 0xFF20..=0xFF26 => self.apu.read_u8(addr), // audio registers
+            0xFF30..=0xFF3F => self.apu.read_u8(addr), // wave ram
+            0xFF40..=0xFF45 | 0xFF47..=0xFF4B => self.ppu.read_u8(addr), // ppu registers
+            0xFF46 => 0xFF, // what does reading from the dma register do?
+            // 0xFF4F => unimplemented!("tried to read CGB VRAM bank select"),
+            // 0xFF50 => unimplemented!("tried to read boot rom flag"),
+            // 0xFF51..=0xFF55 => unimplemented!("tried to read CGB VRAM DMA"),
+            // 0xFF68..=0xFF6B => unimplemented!("tried to read CGB BG/OBJ palettes"),
+            // 0xFF70 => unimplemented!("tried to read CGB WRAM bank select"),
             _ => 0,
         }
     }
@@ -195,12 +126,17 @@ impl Bus {
         match addr {
             0xFF00 => self.joypad.write_u8(val),                // joypad
             0xFF01 | 0xFF02 => self.serial.write_u8(addr, val), // serial data
-            0xFF04..=0xFF07 => self.timer.write_u8(addr, val),
-            0xFF10..=0xFF26 => self.apu.write_u8(addr, val),
-            0xFF30..=0xFF3F => self.apu.write_u8(addr, val),
+            0xFF04..=0xFF07 => self.timer.write_u8(addr, val),  // timer
+            0xFF0F => self.ram_write(addr, val),                // IF
+            0xFF10..=0xFF14 | 0xFF16..=0xFF1E | 0xFF20..=0xFF26 => self.apu.write_u8(addr, val), // audio registers
+            0xFF30..=0xFF3F => self.apu.write_u8(addr, val), // wave ram
+            0xFF40..0xFF46 | 0xFF47..=0xFF4B => self.ppu.write_u8(addr, val), // ppu registers
             0xFF46 => self.do_dma(val), // dma lives in the bus to make things easier
-            0xFF40..0xFF46 | 0xFF47..=0xFF4B => self.ppu.write_u8(addr, val),
-            0xFF0F => self.ram_write(addr, val), // IF
+            // 0xFF4F => unimplemented!("tried to write to CGB VRAM bank select"),
+            // 0xFF50 => unimplemented!("tried to write to boot rom flag"),
+            // 0xFF51..=0xFF55 => unimplemented!("tried to write to CGB VRAM DMA"),
+            // 0xFF68..=0xFF6B => unimplemented!("tried to write to CGB BG/OBJ palettes"),
+            // 0xFF70 => unimplemented!("tried to write to CGB WRAM bank select"),
             _ => (),
         }
     }
